8569a7a2f2f9a8a9585192cdb4b247427aec8144
dallenpyrah <dallenpyrah@gmail.com>
2022-09-25 13:58:12 -0600

    Loving the UI

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,8 +1,10 @@
 import SideBarComponent from "../components/sidebar/SideBarComponent";
-import {useState} from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCancel, faPaperPlane, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function ChatPage() {
-    const [isNewChatModalOpen, setIsNewChatModalOpen] = useState(false);
+    const [message, setMessage] = useState("Write a message...");
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-7 h-screen'>
@@ -10,18 +12,15 @@ export default function ChatPage() {
                 <SideBarComponent />
             </div>
             <div className="col-span-2 p-5 overflow-y-auto">
-                {!isNewChatModalOpen &&
-                    <div className="flex flex-row justify-between">
-                        <h1 className="font-bold text-2xl">Inbox</h1>
-                        <button onClick={() => setIsNewChatModalOpen(true)} className="bg-black text-white pl-3 pr-3 rounded-md hover:bg-gray-800">+</button>
-                    </div>
-                }
-                {isNewChatModalOpen &&
-                    <div className="flex flex-row justify-between">
-                        <input className="w-full" placeholder="Search for a user" />
-                        <button onClick={() => setIsNewChatModalOpen(false)} className="bg-red-400 text-white pl-3 pr-3 rounded-md hover:bg-gray-800">X</button>
-                    </div>
-                }
+                <div className="flex flex-row justify-between">
+                    <h1 className="font-bold text-2xl">Inbox</h1>
+                </div>
+                <div className="flex flex-row mt-2">
+                    <input className="w-full" placeholder="Search for a user" />
+                    <button className="bg-black p-1 ml-2 pr-2 pl-2 text-white rounded-md hover:bg-gray-800">
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
                 <div className="flex flex-row bg-slate-100 p-8 rounded-lg mt-6">
                     <div className="bg-black rounded-md mr-5">Hi</div>
                     <h2>Dallen Pyrah</h2>
@@ -42,19 +41,22 @@ export default function ChatPage() {
 
                 </div>
             </div>
-            <div className="col-span-4 p-5 overflow-y-auto h-full">
-                <div className="flex flex-row h-full">
-                    <div className="flex flex-col justify-between bg-slate-100 w-full overflow-y-auto rounded-lg">
+            <div className="col-span-4 p-5 overflow-y-auto">
+                <div className="flex flex-row h-[95%]">
+                    <div className="flex flex-col bg-slate-100 w-full overflow-y-auto rounded-t-lg justify-between">
                         <div className="flex flex-row justify-between p-5 bg-black">
                             <h1 className="text-md text-white">Gaurav Sharma</h1>
                             <h1 className="text-md text-white">Software Engineer</h1>
                         </div>
-                        <div className="flex flex-row justify-between p-5 bg-slate-100">
-                            <input className="bg-slate-100 w-full" placeholder="Write a message..." />
-                            <button className="text-white text-sm bg-black p-2 rounded-lg ml-3 hover:bg-gray-800">Send</button>
+                        <div className="flex flex-row justify-between break-words">
+                            <span onFocus={() => setMessage("")} contentEditable="true" className="w-screen bg-slate-50 p-4 text-sm">{message}</span>
                         </div>
-
                     </div>
+                </div>
+                <div className="flex flex-row justify-end">
+                    <button className="text-white text-sm bg-black p-2 rounded-lg mt-2 hover:bg-gray-800">
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
                 </div>
             </div>
         </div>
