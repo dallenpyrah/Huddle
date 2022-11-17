e1f4e08467cefe2f0256d0cd1932dd357f531fef
dallenpyrah <dallenpyrah@gmail.com>
2022-11-16 20:48:32 -0700

    Firebase config

--- firebase-config.ts
+++ firebase-config.ts
@@ -2,23 +2,21 @@ import { browserSessionPersistence, getAuth } from 'firebase/auth'
 import * as firebase from 'firebase/app'
 import pino from 'pino'
 
-const logger = pino()
-
 const firebaseConfig = {
   apiKey: 'AIzaSyDo9w_q2OjAF5k2BUv1V_0HNw8wjbLmZYQ',
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  authDomain: 'huddle-68e55.firebaseapp.com',
+  databaseURL: 'https://huddle-68e55-default-rtdb.firebaseio.com',
+  projectId: 'huddle-68e55',
+  storageBucket: 'huddle-68e55.appspot.com',
+  messagingSenderId: '145379649614',
+  appId: '1:145379649614:web:7c2cfb6003d7ea2fe03b1f',
+  measurementId: 'G-N43LD24ZXY'
 }
 
+const logger = pino()
 const app = firebase.initializeApp(firebaseConfig)
 
 const auth = getAuth(app)
-
 auth.onAuthStateChanged((user) => {
   if (user !== null) {
     logger.info(user)
