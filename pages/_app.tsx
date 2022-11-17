809d234f93a1052261701c6ccd3d600a4575fc68
dallenpyrah <dallenpyrah@gmail.com>
2022-11-16 20:15:28 -0700

    Fixed all build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MyGlobalContext } from '../context/GlobalContext'
 import React from 'react'
+import * as dotenv from 'dotenv'
 
 function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
+  dotenv.config()
   const [isAuthenticated, setIsAuthenticated] = React.useState(false)
 
   return (
