ea2603b32b32b00f4a3c2b7024696387e9e87f59
dallenpyrah <dallenpyrah@gmail.com>
2022-11-16 20:18:09 -0700

    Firebase config

---
+++ firebase-config.ts
@@ -0,0 +1,31 @@
+import { browserSessionPersistence, getAuth } from 'firebase/auth'
+import * as firebase from 'firebase/app'
+import pino from 'pino'
+
+const logger = pino()
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+}
+
+const app = firebase.initializeApp(firebaseConfig)
+
+const auth = getAuth(app)
+
+auth.onAuthStateChanged((user) => {
+  if (user !== null) {
+    logger.info(user)
+  } else {
+    logger.info('No user is signed in.')
+  }
+})
+void auth.setPersistence(browserSessionPersistence)
+
+export { auth }
